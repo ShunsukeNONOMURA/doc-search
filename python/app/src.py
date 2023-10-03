59b6e395c22e0e85494cf402d710eba601909ab5
ShunsukeNONOMURA <shunsuke.nonomura@gmail.com>
2023-10-03 12:07:26 +0900

    add sentence bert sample

--- python/app/src.py
+++ python/app/src.py
@@ -1,1 +1,27 @@
+from text_encoder import TextEncoder
+from sentence_transformers import util
+text_encoder = TextEncoder()
+
+# 入力文をベクトル表現に変換
+sentence = '今日は雨振らなくてよかった'
+embedding = text_encoder.model.encode(sentence, convert_to_tensor=True)
+
+# print(embedding)
+
+# 検索対象文をベクトル表現に変換
+sentences = [
+    '好きな食べ物は何ですか?',
+    'どこにお住まいですか?',
+    '朝の電車は混みますね',
+    '今日は良いお天気ですね',
+    '最近景気悪いですね']
+embeddings = text_encoder.model.encode(sentences, convert_to_tensor=True)
+
+# 入力文と検索対象文のベクトル表現の類似度を計算
+scores = util.pytorch_cos_sim(embedding, embeddings)
+predicted_idx = scores.argmax(1).item() # スコアが最大のインデックスの取得
+print('文:', sentence)
+print('類似文:', sentences[predicted_idx])
+print('類似度:', scores)
+
 print(0)
---
+++ python/app/text_encoder.py
@@ -0,0 +1,25 @@
+import logging
+import os
+
+import jaconv
+from sentence_transformers import SentenceTransformer
+
+
+class TextEncoder:
+    def __init__(self, model_path=None):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+        handler = logging.NullHandler()
+        handler.setLevel(logging.DEBUG)
+        self.logger.addHandler(handler)
+        # self.model = SentenceTransformer('stsb-xlm-r-multilingual')
+        self.model = SentenceTransformer('./pytorch_model.bin')
+
+    def encode(self, text, normalize=True):
+        if normalize:
+            text = jaconv.normalize(text, "NFKC")
+
+        vector = self.model.encode(text)
+        self.logger.debug(vector.shape)
+
+        return vector
